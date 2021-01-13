f67f77c9bf1f07d41fa598dfc54a01c6309481af
bodhigit <76668852+bodhigit@users.noreply.github.com>
2021-01-13 13:45:15 +0530

    Add files via upload

---
+++ runPython.bat
@@ -0,0 +1,6 @@
+@echo off
+cmd /k "cd /d F:\Python2020\BodhiTechnology_workPlace\bodhi_Website\bidhiWebsite\venv\scripts & activate & cd /d    F:\Python2020\BodhiTechnology_workPlace\bodhi_Website\bidhiWebsite\bodhiwebsite & python manage.py runserver"
+
+
+
+
